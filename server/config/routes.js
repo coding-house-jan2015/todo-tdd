a9ec36e81dd5262bf4f3e8a57a91677acfc4cf30
Chyld Medford <chyld.medford@gmail.com>
2015-02-24 14:39:32 -0800

    item create

--- server/config/routes.js
+++ server/config/routes.js
@@ -9,5 +9,6 @@ module.exports = [
   {method: 'post', path: '/users/authenticate', config: require('../routes/users/authenticate')},
   {method: 'post', path: '/logout', config: require('../routes/users/logout')},
 
-  {method: 'get', path: '/items/new', config: require('../routes/items/new')}
+  {method: 'get', path: '/items/new', config: require('../routes/items/new')},
+  {method: 'post', path: '/items', config: require('../routes/items/create')}
 ];
--- test/acceptance/items.js
+++ test/acceptance/items.js
@@ -10,12 +10,15 @@ var describe = lab.describe;
 var it = lab.it;
 var beforeEach = lab.beforeEach;
 var server = require('../../server/index');
+
 var cookie;
+var bob;
 
 describe('items', function() {
   beforeEach(function(done) {
     User.remove(function() {
-      User.register({email:'bob@aol.com', password:'123'}, function(){
+      User.register({email:'bob@aol.com', password:'123'}, function(err, user){
+        bob = user;
         var options = {
           method:'post',
           url:'/users/authenticate',
@@ -44,6 +47,49 @@ describe('items', function() {
       server.inject(options, function(response) {
         expect(response.statusCode).to.equal(200);
         expect(response.payload).to.include('New Item');
+        done();
+      });
+    });
+  });
+
+  describe('post /items', function() {
+    it('should create a new item', function(done) {
+      var options = {
+        method:'post',
+        url:'/items',
+        payload: {
+          title: 'a',
+          due: '2008-11-03',
+          tags: 'b,c,d',
+          priority: 'e'
+        },
+        headers: {
+          cookie: cookie
+        }
+      };
+      server.inject(options, function(response) {
+        expect(response.statusCode).to.equal(302);
+        expect(response.headers.location).to.include('/items');
+        done();
+      });
+    });
+
+    it('should NOT create a new item - failed joi validation', function(done) {
+      var options = {
+        method:'post',
+        url:'/items',
+        payload: {
+          title: '',
+          due: '',
+          tags: '',
+          priority: ''
+        },
+        headers: {
+          cookie: cookie
+        }
+      };
+      server.inject(options, function(response) {
+        expect(response.statusCode).to.equal(400);
         done();
       });
     });
